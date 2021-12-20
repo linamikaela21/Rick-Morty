5bbf244123ea439ab40d92ec5ffa83c1066b6680
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-12-20 17:24:24 -0300

    Vamos a ver si ahora deployas

--- backend-rick-and-morty/config.js
+++ backend-rick-and-morty/config.js
@@ -1,4 +1,6 @@
 const config = {
+  //Siempre en deploy tiene que ir con false ! 
+  //Sino puedo poner TRUE borrar la BD y volver a subirlo con false si necesito borrar toda la BD
     force: false
   }
   
