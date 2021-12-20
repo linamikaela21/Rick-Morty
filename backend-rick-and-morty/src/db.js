c60aaa93d0674f2db5a79db972b5f90c6e833c70
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-12-20 17:37:43 -0300

    Vamos a ver si ahora deployas

--- backend-rick-and-morty/src/db.js
+++ backend-rick-and-morty/src/db.js
@@ -6,8 +6,7 @@ const path = require('path')
 const { DB_PG_USER, DB_PG_PASSWORD, DB_PG_HOST, DB_PG_PORT, DB_PG_DATABASE, DATABASE_URL } = process.env
 
 const isProduction = process.env.NODE_ENV === 'production'
-//const connectionString = isProduction ? DATABASE_URL : `postgres://${DB_PG_USER}:${DB_PG_PASSWORD}@${DB_PG_HOST}:${DB_PG_PORT}/${DB_PG_DATABASE}`
-const connectionString = isProduction ? DATABASE_URL : `postgres://${DB_PG_USER}:${DB_PG_PASSWORD}@${DB_PG_HOST}:5432/${DB_PG_DATABASE}`
+const connectionString = isProduction ? DATABASE_URL : `postgres://${DB_PG_USER}:${DB_PG_PASSWORD}@${DB_PG_HOST}:${DB_PG_PORT}/${DB_PG_DATABASE}`
 
 const sequelize = new Sequelize(connectionString, {
   logging: false, // set to console.log to see the raw SQL queries
