44b0bcd7b431633831448844e39ea17de8eb5fc5
Lina Gutierrez Arribas <mikaelagutierrezarribas@gmail.com>
2021-12-20 19:57:59 -0300

    Modifique la URL

--- frontend-rick-and-morty/src/config/config.js
+++ frontend-rick-and-morty/src/config/config.js
@@ -1,8 +1,8 @@
-export const URL_BASE = process.env.DATABASE_URL + '/api' || 'http://localhost:3001/api'
+export const URL_BASE = process.env.DATABASE_URL || 'http://localhost:3001'
 
-export const URL_ALL_CHARACTERS = URL_BASE + '/characters'
-export const URL_ALL_EPISODES = URL_BASE + '/episodes'
-export const URL_ALL_LOCATIONS = URL_BASE + '/locations'
+export const URL_ALL_CHARACTERS = URL_BASE + '/api/characters'
+export const URL_ALL_EPISODES = URL_BASE + '/api/episodes'
+export const URL_ALL_LOCATIONS = URL_BASE + '/api/locations'
 
 export const URL_CHARACTERS_NAME = URL_ALL_CHARACTERS + '/?name='
 export const URL_EPISODES_NAME = URL_ALL_EPISODES + '/?name='
